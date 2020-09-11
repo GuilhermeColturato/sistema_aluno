b364ca327cec00879ed9db93ca2fdee3c7058320
GuilhermeColturato <guilhermecolturato13@gmail.com>
2020-09-11 03:42:05 -0300

    Nesse commit foi desenvolvido a funcionalidade de inserir alunos no banco de dados. Foram criados varios arquivos

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+	include("cabecalho.inc.php" );
+?>
+	<a href ="cad_alunos.php"> Cadastro de Alunos </a><br>
+	<a href ="cons_todos_alunos.php"> Consultar TODOS OS Alunos </a><br>
+	<a href ="cons_ra_alunos.php"> Consultar Alunos POR RA </a><br>
+<?php
+	include("rodape.inc.php");
+?>
